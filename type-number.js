043e405efad088a0f9ccb89c40401cc3125adf5d
Renatta Pereira <renatta.lll@hotmail.com>
2022-10-18 16:53:58 -0300

    exemplos da aula 01

---
+++ type-number.js
@@ -0,0 +1,17 @@
+// tipo número
+
+const meuNumero = 3;
+
+const primeiroNumero = 1;
+const segundoNumero = 2;
+
+const operacaoMatematica = primeiroNumero + segundoNumero;
+
+console.log(operacaoMatematica);
+
+const pontoFlutuante = 3.3;
+const pontoFlutuanteSemZero = 0.5;
+
+const novaOperacao = primeiroNumero / pontoFlutuanteSemZero;
+
+console.log(novaOperacao);
